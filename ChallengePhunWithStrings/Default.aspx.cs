2b3b255735941ef4d3109b1e0f84857ee7db3f30
MikeyUchiha <mikey@starfantasygames.com>
2015-06-20 16:14:13 -0700

    Initial Commit

---
+++ ChallengePhunWithStrings/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengePhunWithStrings
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // 1. Reverse your name
+            string name = "Malik Gray";
+            // In my case, the result would be:
+            // yarG kilaM
+
+            ResultLabel.Text = name;
+        }
+    }
+}

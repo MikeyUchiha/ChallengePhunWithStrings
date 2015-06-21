dd9d89c560ee7e980fb32a23b1f5fedb5a999b5e
MikeyUchiha <mikey@starfantasygames.com>
2015-06-20 17:30:20 -0700

    Completed third challenge
    Add padding to the left and right side of a comma delimited string of
    names that total 14. Place each name on a separate line.

--- ChallengePhunWithStrings/Default.aspx.cs
+++ ChallengePhunWithStrings/Default.aspx.cs
@@ -26,9 +26,10 @@ namespace ChallengePhunWithStrings
             //ResultLabel.Text = name;
 
             // 2. Reverse this sequence:
-            string names = "Luke,Leia,Han,Chewbaca";
+            string names = "Luke,Leia,Han,Chewbacca";
             // When you're finished it should look like this:
             // Chewbacca,Han,Leia,Luke
+            
             //string[] namearray = names.Split(',');
             //string temp = "";
             //for(int i = namearray.Length - 1; i >= 0; i--)
@@ -36,6 +37,24 @@ namespace ChallengePhunWithStrings
             //    temp += namearray[i] + ",";
             //}
             //names = temp.Remove(temp.Length - 1);
+            //ResultLabel.Text = names;
+
+            // 3. Use the sequence to create ascii art:
+            // *****Luke*****
+            // *****Leia*****
+            // *****Han******
+            // **Chewbacca***
+
+            //string[] namearray = names.Split(',');
+            //string result = "";
+            //for(int i = 0; i < namearray.Length; i++)
+            //{
+            //    int trimAmount = (14 - namearray[i].Length)/2;
+            //    string paddedString = namearray[i].PadLeft(namearray[i].Length + trimAmount, '*');
+            //    result += paddedString.PadRight(14, '*');
+            //    result += "<br/>";
+            //}
+            //names = result;
             //ResultLabel.Text = names;
 
 
